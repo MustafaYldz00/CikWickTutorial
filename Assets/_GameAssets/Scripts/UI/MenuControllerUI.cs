cbb08bf9c2c6effde47557408d13166b7787d9bb
DreamBit Game <103414856+MustafaYldz00@users.noreply.github.com>
2025-06-27 16:48:50 +0300

    v0.21 Main Menu Added

---
+++ Assets/_GameAssets/Scripts/UI/MenuControllerUI.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuControllerUI : MonoBehaviour
+{
+    [SerializeField] private Button _playButton;
+    [SerializeField] private Button _QuitButton;
+
+    private void Awake()
+    {
+        _playButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(Consts.SceneNames.GAME_SCENE);
+        });
+        _QuitButton.onClick.AddListener(() =>
+        {
+            Application.Quit();
+        });
+    }
+}
